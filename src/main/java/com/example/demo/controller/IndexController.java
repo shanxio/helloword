d5c369815f88e9f4af3694194521793b48c9983a
sam <2562225391@qq.com>
2020-03-25 17:25:59 +0800

    测试

---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
